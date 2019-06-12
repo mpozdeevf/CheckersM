3628238117d6e26f963b97241e7ff333c7c8495e
mpozdeevf <39208878+mpozdeevf@users.noreply.github.com>
2019-06-12 09:32:51 +0400

    Test SignalR hub

---
+++ CheckersM/CheckersM/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CheckersM.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
