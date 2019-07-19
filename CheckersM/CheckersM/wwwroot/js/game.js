16c3ae099d1a37d8b1495459667449f80e685707
mpozdeevf <mpozdeevf@gmail.com>
2019-07-19 22:06:56 +0300

    singleplayer added

--- CheckersM/CheckersM/wwwroot/js/game.js
+++ CheckersM/CheckersM/wwwroot/js/game.js
@@ -4,11 +4,18 @@ var connection = new signalR.HubConnectionBuilder().withUrl("/game").build();
 
 var counter = 0;
 
+var isUserTurn = false;
+
+var possiblePositions;
+var currentPosition;
+
 document.getElementById("startButton").disabled = true;
 
 connection.on("Start", function (json) {
     var obj = JSON.parse(json);
-    setTimeout(drawBoard, 1500, obj.Position, obj.PossiblePositions);
+    possiblePositions = obj.PossiblePositions;
+    currentPosition = obj.Position;
+    setTimeout(drawBoard, 500);
 });
 
 connection.on("End", function (message) {
@@ -30,18 +37,17 @@ startButton.addEventListener("click", function (event) {
     event.preventDefault();
 });
 
-function drawBoard(position, positions) {
-    for (var i = 0; i < position.length; i++) {
-        setTimeout(drawCheckers, counter * 500, position[i]);
+function drawBoard() {
+    for (var i = 0; i < currentPosition.length; i++) {
+        setTimeout(drawCheckers, 0, currentPosition[i]);
         counter++;
     }
-    play(positions);
+    play();
 }
 
 function drawClientBoard(position, board) {
     for (var i = 0; i < position.length; i++) {
-        setTimeout(drawCheckers, counter * 500, position[i]);
-        counter++;
+        drawCheckers(position[i]);
     }
     connection.invoke("PlayGame", board).catch(function (err) {
         return console.error(err.toString());
@@ -59,10 +65,16 @@ function clearBoard() {
 function drawCheckers(str) {
     clearBoard();
     for (var i = 0; i < str.length; i++) {
+        if (str[i] === "e") {
+            var id1 = Math.floor(i / 8).toString() + (i % 8).toString();
+            var e1 = document.getElementById(id1);
+            e1.onclick = cellMoveOnClick;
+        }
         if (str[i] !== "e") {
             var id = Math.floor(i / 8).toString() + (i % 8).toString();
             var e = document.getElementById(id);
             if (str[i] === 'w' || str[i] === 'W') {
+                e.onclick = cellOnClick;
                 e.style.color = 'yellow';
             } else {
                 e.style.color = 'black';
@@ -72,12 +84,57 @@ function drawCheckers(str) {
     }
 }
 
-function play(positions) {
-    var n = Math.floor(Math.random() * positions.length);
-    var board = positions[n][positions[n].length - 1];
-    setTimeout(drawClientBoard, 1500, positions[n], board);
+function play() {
+    // var n = Math.floor(Math.random() * possiblePositions.length);
+    // var board = possiblePositions[n][possiblePositions[n].length - 1];
+    isUserTurn = true;
+    //setTimeout(drawClientBoard, 1500, possiblePositions[n], board);
 }
 
 function endOfTheGame(message) {
     alert(message);
+}
+
+var newPossiblePositions;
+var isMoveCheckerChosen = false;
+
+function cellOnClick(e) {
+    var cell = e.target;
+    if (isUserTurn) {
+        cell.style.color = 'orange';
+        newPossiblePositions = [];
+        var id = parseInt(cell.id[0]) * 8 + parseInt(cell.id[1]);
+        for (var i = 0; i < possiblePositions.length; i++) {
+            if (possiblePositions[i][0][id] === 'e') {
+                newPossiblePositions.push(possiblePositions[i]);
+            }
+        }
+        isMoveCheckerChosen = true;
+    }
+}
+
+var tempPossiblePositions;
+
+function cellMoveOnClick(e) {
+    var cell = e.target;
+    var id = parseInt(cell.id[0]) * 8 + parseInt(cell.id[1]);
+    if (isMoveCheckerChosen) {
+        isMoveCheckerChosen = false;
+        isUserTurn = false;
+        tempPossiblePositions = [];
+        for (var i = 0; i < newPossiblePositions.length; i++) {
+            if (newPossiblePositions[i][0][id] === 'w' || newPossiblePositions[i][0][id] === 'W') {
+                tempPossiblePositions.push(newPossiblePositions[i]);
+            }
+        }
+        if (tempPossiblePositions.length === 0) {
+            alert("Move is impossible");
+        } //else if (tempPossiblePositions[0].length === 1) {
+        //     drawCheckers(tempPossiblePositions[0][0]);
+        // } else {
+        //     dr
+        // }
+        var board = tempPossiblePositions[0][tempPossiblePositions[0].length - 1];
+        drawClientBoard(tempPossiblePositions[0], board)
+    }
 }
